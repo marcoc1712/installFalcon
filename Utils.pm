47aebcd1dfa6f013c0b3000491ce993ec8362678
marcoc1712 <marcoc1712@gmail.com>
2017-03-20 04:22:33 +0100

    new installer

--- Utils.pm
+++ Utils.pm
@@ -338,6 +338,13 @@ sub getTimeString {
     my $self = shift;
     my $time = shift || time;
     
+    return POSIX::strftime('%Y%m%d%H%M%S', localtime($time));
+    
+}
+sub getNiceTimeString {
+    my $self = shift;
+    my $time = shift || time;
+    
     return POSIX::strftime('%Y/%m/%d %H:%M:%S', localtime($time));
     
 }
